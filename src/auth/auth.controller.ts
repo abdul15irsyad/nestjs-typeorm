bdb44557bb85f3ffaba47cb325cf4ebd59b02284
Irsyad Abdul <abdulirsyad15@gmail.com>
2023-09-12 02:27:20 +0700

    passport-jwt

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,12 +1,23 @@
-import { Controller, Post, Request, UseGuards } from '@nestjs/common';
+import { Controller, Get, Post, Request, UseGuards } from '@nestjs/common';
 import { AuthGuard } from '@nestjs/passport';
 import { User } from '../user/entities/user.entity';
+import { JwtService } from '@nestjs/jwt';
 
 @Controller('auth')
 export class AuthController {
+  constructor(private readonly jwtService: JwtService) {}
+
   @UseGuards(AuthGuard('local'))
   @Post('login')
   async login(@Request() req: Request & { user: User }) {
+    return {
+      accessToken: this.jwtService.sign({ id: req.user.id }),
+    };
+  }
+
+  @UseGuards(AuthGuard('jwt'))
+  @Get('profile')
+  async profile(@Request() req: Request & { user: { id: string } }) {
     return req.user;
   }
 }
--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -4,10 +4,19 @@ import { UserModule } from '../user/user.module';
 import { PassportModule } from '@nestjs/passport';
 import { LocalStrategy } from './local.strategy';
 import { AuthController } from './auth.controller';
+import { JwtModule } from '@nestjs/jwt';
+import { JwtStrategy } from './jwt.strategy';
 
 @Module({
-  imports: [forwardRef(() => UserModule), PassportModule],
+  imports: [
+    forwardRef(() => UserModule),
+    PassportModule,
+    JwtModule.register({
+      secret: 'SuperDuperSecretKey',
+      signOptions: { expiresIn: '2h' },
+    }),
+  ],
   controllers: [AuthController],
-  providers: [AuthService, LocalStrategy],
+  providers: [AuthService, LocalStrategy, JwtStrategy],
 })
 export class AuthModule {}
